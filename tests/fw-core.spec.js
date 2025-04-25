45ae4e11fe10bd8b015b8cf519cee69e61e724ed
puranjaysharma <pursharma.cse16@chitkarauniversity.edu.in>
2025-04-25 20:09:31 +0530

    code cleaning intest files, in config file added slowmo field

--- tests/fw-core.spec.js
+++ tests/fw-core.spec.js
@@ -1,4 +1,4 @@
-// login.spec.ts
+// login & Sign Up Scenarios
 
 import { test, expect } from '@playwright/test';
 
@@ -9,157 +9,84 @@ test.beforeEach(async ({ page }) => {
 
 test('Login for the Diamond', async ({ page }) => {
     const signInLink = page.locator('a', { hasText: 'Sign in' });
-
     const visibleElement = signInLink.locator(':visible').first();
-
     if (await visibleElement.isVisible()) {
-     // Now you can interact with the visible element, e.g., click it
      await visibleElement.click();
     }
-
-    // Locate the input field inside a div (you can refine this with a more specific selector)
-    const inputField = await page.locator('div input[type="email"]'); // Adjust the selector if needed
-
-    // Type an email into the input field
+    const inputField = await page.locator('div input[type="email"]');
     await inputField.fill('diamond.ns@yopmail.com');
-  
-    // Optionally, verify that the field contains the correct email
     await expect(inputField).toHaveValue('diamond.ns@yopmail.com');
-
     const continueButton = await page.locator('input[type="submit"][value="Continue"]');
-
     await continueButton.click();
-
-    const inputPassword = await page.locator('div input[type="Password"]'); // Adjust the selector if needed
-
-    // Type an email into the input field
+    const inputPassword = await page.locator('div input[type="Password"]');
     await inputPassword.fill('Test@123');
-
     const signInButton = await page.locator('input[type="submit"][value="Sign in"]');
-
     await signInButton.click();
-
-    await page.waitForTimeout(5000); // make script wait on the Diamond Home
-
-    //await expect(page).toHaveTitle(/Diamond/);
+    await page.waitForTimeout(5000);
 
 });
 
 test('Login for the Red', async ({ page }) => {
       const signInLink = page.locator('a', { hasText: 'Sign in' });
-      
       const visibleElement = signInLink.locator(':visible').first();
-
       if (await visibleElement.isVisible()) {
-        // Now you can interact with the visible element, e.g., click it
         await visibleElement.click();
       }
       //await signInLink.click();
-
-      // Locate the input field inside a div (you can refine this with a more specific selector)
-      const inputField = await page.locator('div input[type="email"]'); // Adjust the selector if needed
-  
-      // Type an email into the input field
+      const inputField = await page.locator('div input[type="email"]');
       await inputField.fill('red.ns@yopmail.com');
-    
-      // Optionally, verify that the field contains the correct email
       await expect(inputField).toHaveValue('red.ns@yopmail.com');
-  
-      const continueButton = await page.locator('input[type="submit"][value="Continue"]');
-  
-      await continueButton.click();
-  
-      const inputPassword = await page.locator('div input[type="Password"]'); // Adjust the selector if needed
-  
-      // Type an email into the input field
+      const continueButton = await page.locator('input[type="submit"][value="Continue"]');  
+      await continueButton.click(); 
+      const inputPassword = await page.locator('div input[type="Password"]');
       await inputPassword.fill('Test@123');
-  
       const signInButton = await page.locator('input[type="submit"][value="Sign in"]');
-  
       await signInButton.click();
-  
-      await page.waitForTimeout(5000); // make script wait on the Diamond Home
+      await page.waitForTimeout(5000);
       //await expect(heading).toHaveText('WELCOME BACK', { timeout: 10000 });
       //await expect(page).toHaveTitle("Women's Club For Personal And Professional Success - FW");
   
   });
 
 test('Login for the Gold', async ({ page }) => {
-
       const signInLink = page.locator('a', { hasText: 'Sign in' });
-
       const visibleElement = signInLink.locator(':visible').first();
-
       if (await visibleElement.isVisible()) {
-       // Now you can interact with the visible element, e.g., click it
       await visibleElement.click();
       }
-  
-      // Locate the input field inside a div (you can refine this with a more specific selector)
-      const inputField = await page.locator('div input[type="email"]'); // Adjust the selector if needed
-  
-      // Type an email into the input field
+      const inputField = await page.locator('div input[type="email"]');
       await inputField.fill('gold.ns@yopmail.com');
-    
-      // Optionally, verify that the field contains the correct email
-      await expect(inputField).toHaveValue('gold.ns@yopmail.com');
-  
+      await expect(inputField).toHaveValue('gold.ns@yopmail.com'); 
       const continueButton = await page.locator('input[type="submit"][value="Continue"]');
-  
       await continueButton.click();
-  
-      const inputPassword = await page.locator('div input[type="Password"]'); // Adjust the selector if needed
-  
-      // Type an email into the input field
-      await inputPassword.fill('Test@123');
-  
+      const inputPassword = await page.locator('div input[type="Password"]');
+      await inputPassword.fill('Test@123'); 
       const signInButton = await page.locator('input[type="submit"][value="Sign in"]');
-  
-      await signInButton.click();
-  
-      await page.waitForTimeout(5000); // make script wait on the Diamond Home
-  
+      await signInButton.click(); 
+      await page.waitForTimeout(5000);
       //await expect(page).toHaveTitle(/Diamond/);
   
   });
 
 
 test('New Member Register', async ({ page }) => {
-      const joinNowlink = page.locator('a', { hasText: 'Join Now' });
-  
-      await joinNowlink.click();
-  
-      const indivisualBtn = await page.locator('div input[type="For Individuals"]'); 
-  
+      const joinNowlink = page.locator('a', { hasText: 'Join Now' }); 
+      await joinNowlink.click(); 
+      const indivisualBtn = await page.locator('div input[type="For Individuals"]');  
       await indivisualBtn.click;
-
       await page.goto('https://lewinnovation.memberful.com/checkout?plan=103012');
-
       const buttonReg = page.locator('button', { hasText: 'Add coupon' });
-
       buttonReg.click();
-
       const inputCupon = page.locator('input[aria-label="Coupon code"]');
-
       await inputCupon.fill('DEVTEST1234');
-
-      const applyButton = page.locator('button', { hasText: 'Apply' });
-      
+      const applyButton = page.locator('button', { hasText: 'Apply' });      
       applyButton.click();
-
       const fullName = page.locator('input[name="checkout[full_name]"]');
       await fullName.fill('Puranjay');
-
-      //const email = page.locator('input[aria-label="Email address"]');
       const email = page.locator('input[placeholder="Email address"]');
-
       await email.fill('puranjay@gmail.com');
-
       const orderButton = page.locator('button', { hasText: 'Place your order' });
-      
       await orderButton.click();
-
-
   
   });
 
--- tests/fw-events.spec.js
+++ tests/fw-events.spec.js
@@ -2,15 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test('Booking an Event for Red', async ({ page }) => {
   await page.goto('https://fwstaging-core.futurewomen.com/');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('mailto:red.ns@yopmail.com');
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).press('ArrowLeft');
-  await page.getByRole('textbox', { name: 'Email' }).fill('red.ns@yopmail.com');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Test@123');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  const signInLink = page.locator('a', { hasText: 'Sign in' });
+  const visibleElement = signInLink.locator(':visible').first();
+  if (await visibleElement.isVisible()) {
+    await visibleElement.click();
+  }
+  const inputField = await page.locator('div input[type="email"]');
+  await inputField.fill('red.ns@yopmail.com');
+  await expect(inputField).toHaveValue('red.ns@yopmail.com');
+  const continueButton = await page.locator('input[type="submit"][value="Continue"]');
+  await continueButton.click();
+  const inputPassword = await page.locator('div input[type="Password"]');
+  await inputPassword.fill('Test@123');
+  const signInButton = await page.locator('input[type="submit"][value="Sign in"]');
+  await signInButton.click();
   await page.goto('https://fwstaging-core.futurewomen.com/home/welcome-red/');
   await page.getByRole('link', { name: 'YOUR EVENTS' }).click();
   await page.goto('https://fwstaging-community.futurewomen.com/events/red-member-events/');
@@ -21,22 +26,30 @@ test('Booking an Event for Red', async ({ page }) => {
   await page.locator('div').filter({ hasText: /^Confirm$/ }).locator('span').click();
   await page.locator('#am-add-to-calendar div').filter({ hasText: 'Add to Calendar' }).click();
   await page.getByText('Finish').click();
-  await page.getByText('Apr 29 PS - Test Automation Event 2025 Free 2/10 7:45PM - 8:45PM').click();
-  await page.getByRole('button', { name: 'Close' }).click();
+
 });
 
 test('Register a Diamond Course', async ({ page }) => {
   await page.goto('https://fwstaging-core.futurewomen.com/');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('diamond.ns@yopmail.com');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Test@123');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  const signInLink = page.locator('a', { hasText: 'Sign in' });
+  const visibleElement = signInLink.locator(':visible').first();
+  if (await visibleElement.isVisible()) {
+   await visibleElement.click();
+  }
+  const inputField = await page.locator('div input[type="email"]');
+  await inputField.fill('diamond.ns@yopmail.com');
+  await expect(inputField).toHaveValue('diamond.ns@yopmail.com');
+  const continueButton = await page.locator('input[type="submit"][value="Continue"]');
+  await continueButton.click();
+  const inputPassword = await page.locator('div input[type="Password"]');
+  await inputPassword.fill('Test@123');
+  const signInButton = await page.locator('input[type="submit"][value="Sign in"]');
+  await signInButton.click();
   await page.goto('https://fwstaging-core.futurewomen.com/home/welcome-diamond/');
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
   await page.locator('.event-button').first().click();
+  
 });
